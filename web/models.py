1ae7aa457e68b0a2eaf3d248effd6105fd8ee6b8
Grystniu <maglesha200@gmail.com>
2023-11-23 22:03:28 +0500

    +database +postgreSQL

--- web/models.py
+++ web/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
+
+User = get_user_model()
+
+
+class movie(models.Model):
+    movie_name = models.CharField(max_length=256)
+    gener = models.CharField(max_length=256)
+
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=256)
+    password = models.CharField(max_length=256)
+    movie_id = models.ManyToManyField(movie)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
