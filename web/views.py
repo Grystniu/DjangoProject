1bb89db230f8c5f6cd479875aaf03843bbe01452
Grystniu <maglesha200@gmail.com>
2023-11-24 17:15:05 +0500

    +urls +index.html

--- web/views.py
+++ web/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
+
+
+def main_view(request):
+    year = datetime.now().year
+    return render(request, 'web/index.html', {
+        'year': year,
+    })
